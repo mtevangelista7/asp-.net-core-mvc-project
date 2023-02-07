438ca173acf870936590ff16b98219fb1c17e34b
Matheus Evangelista Dos Santos <evangelista.dev1920@gmail.com>
2023-02-07 20:48:05 -0300

    adicionado o scaffolding e o entity framework no projeto

---
+++ SistemaWebCoreMvc/SistemaWebCoreMvc/Data/SistemaWebCoreMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaWebCoreMvc.Models;
+
+namespace SistemaWebCoreMvc.Data
+{
+    public class SistemaWebCoreMvcContext : DbContext
+    {
+        public SistemaWebCoreMvcContext (DbContextOptions<SistemaWebCoreMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemaWebCoreMvc.Models.Departamento> Departamento { get; set; } = default!;
+    }
+}
--- SistemaWebCoreMvc/SistemaWebCoreMvc/Program.cs
+++ SistemaWebCoreMvc/SistemaWebCoreMvc/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaWebCoreMvc.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<SistemaWebCoreMvcContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SistemaWebCoreMvcContext") ?? throw new InvalidOperationException("Connection string 'SistemaWebCoreMvcContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
